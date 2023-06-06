bcd0f250c979312ef636c72dbddae85d4086c654
TaylorHudson <taylorhudsonsantos@gmail.com>
2023-06-06 15:25:52 -0300

    feat: adding DTO Support in Customer Service and Controller.

--- ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/service/CustomerService.java
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/service/CustomerService.java
@@ -1,9 +1,12 @@
 package br.com.compassuol.sp.challenge.ecommerce.service;
 
 
+import br.com.compassuol.sp.challenge.ecommerce.dto.request.CustomerRequestDTO;
+import br.com.compassuol.sp.challenge.ecommerce.dto.response.CustomerResponseDTO;
 import br.com.compassuol.sp.challenge.ecommerce.entity.Customer;
 import br.com.compassuol.sp.challenge.ecommerce.exceptions.ResourceNotFoundException;
 import br.com.compassuol.sp.challenge.ecommerce.repository.CustomerRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +15,19 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, ModelMapper mapper) {
         this.customerRepository = customerRepository;
+        this.mapper = mapper;
     }
 
-    public Customer findCustomerById(int id) {
-        return customerRepository.findById(id)
+    public CustomerResponseDTO findCustomerById(int id) {
+        Customer customer = customerRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Did not find customer with id - " + id));
+
+        return mapper.map(customer, CustomerResponseDTO.class);
     }
 
     public Customer createCustomer(Customer customer) {
@@ -28,12 +36,14 @@ public class CustomerService {
         return createdCustomer;
     }
 
-    public Customer updateCustomer(int id, Customer customer) {
+    public CustomerResponseDTO updateCustomer(int id, CustomerRequestDTO request) {
         customerRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("The id supplied must be from a customer that is already created"));
 
+        Customer customer = mapper.map(request, Customer.class);
         customer.setCustomerId(id);
-        return customerRepository.save(customer);
+        Customer updatedCustomer = customerRepository.save(customer);
+        return mapper.map(updatedCustomer, CustomerResponseDTO.class);
     }
 
 }

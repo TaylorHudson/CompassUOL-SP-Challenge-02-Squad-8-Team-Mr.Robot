e85884356ae6272dfba1826b88175dab880e620b
Vitor Hugo <vitorhugodias72@gmail.com>
2023-06-07 15:15:13 -0300

    Revert "feat: adding Product script.sql and RestCRUD Methods (GET, POST)"

--- ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/exceptions/GlobalErrorHandler.java
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/exceptions/GlobalErrorHandler.java
@@ -1,14 +1,14 @@
 package br.com.compassuol.sp.challenge.ecommerce.exceptions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalErrorHandler {
--- ecommerce/src/main/resources/DBScripts/productTable.sql
+++
@@ -1,9 +0,0 @@
-
-create table if not exists Product(
-
-productId int auto_increment primary key,
-name VARCHAR(150) NOT NULL,
-price DECIMAL(10, 4) NOT NULL,
-description TEXT NOT NULL
-
-);
